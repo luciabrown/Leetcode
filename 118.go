40983db4c1cb49eee982ad1ccbe9d4760f80ea32
Lucia Brown <luciabrown1015@gmail.com>
2025-08-01 21:08:03 +0100

    118. Pascal's Triangle

---
+++ 118.go
@@ -0,0 +1,52 @@
+// 118. Pascal's Triangle
+import "fmt"
+func generate(numRows int) [][]int {
+	nodeLevels := BuildPascalTree(1, numRows) 
+	result := make([][]int, len(nodeLevels))   // initialise 2D slice
+
+	for i, level := range nodeLevels {
+		row := make([]int, len(level))      // length of the level
+		for j, node := range level {
+			row[j] = node.Value             // populate level with values
+		}
+		result[i] = row
+	}
+	return result 
+}
+
+// Node structure
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+func BuildPascalTree(rootVal int, N int) [][]*Node {
+	if N <= 0 {
+		return [][]*Node{}
+	}
+	// Initialize first level with root
+	root := &Node{Value: rootVal}
+	levels := [][]*Node{{root}}
+
+	for level := 1; level < N; level++ {
+		prev := levels[level-1]
+		curr := make([]*Node, 0, level+1)
+		for i := 0; i <= level; i++ {
+			var val int
+			// Left edge (only one parent)
+			if i == 0 {
+				val = prev[0].Value
+			// Right edge (only one parent)
+			} else if i == level {
+				val = prev[len(prev)-1].Value
+			// Middle (sum of two parents)
+			} else {
+				val = prev[i-1].Value + prev[i].Value
+			}
+			curr = append(curr, &Node{Value: val})
+		}
+		levels = append(levels, curr)
+	}
+	return levels
+}
