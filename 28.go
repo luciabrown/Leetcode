9f1adf1744dcda6d66d0f63f30c171ffa89b4ac3
Lucia Brown <luciabrown1015@gmail.com>
2025-08-03 11:55:09 +0100

    28. Find the Index of the First Occurrence in a String

--- 28.go
+++ 28.go
@@ -1,15 +1,6 @@
 // 28. Find the Index of the First Occurrence in a String
-
 import "strings"
 
 func strStr(haystack string, needle string) int {
-    if !strings.Contains(haystack,needle){
-        return -1
-    }
-    var currentIndex int
-    currentIndex = 0
-    for len(haystack){
-        // TODO
-    }
-    
+    return strings.Index(haystack, needle) 
 }
