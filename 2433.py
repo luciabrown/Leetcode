7bc7f1447e20ee2d62f02a3cff54de28b7291eee
Lucia Brown <luciabrown1015@gmail.com>
2025-09-16 21:12:53 +0100

    2433. Find The Original Array of Prefix Xor

---
+++ 2433.py
@@ -0,0 +1,9 @@
+# 2433. Find The Original Array of Prefix Xor
+
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        result=[0]*len(pref)
+        result[0]=pref[0]
+        for i in range(1,len(pref)):
+            result[i]=pref[i-1]^pref[i]
+        return result
