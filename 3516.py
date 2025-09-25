6be49610f2652c0e218b47ab1144e3d71640a4e6
Lucia Brown <luciabrown1015@gmail.com>
2025-09-25 12:59:53 +0100

    3516. Find Closest Person

---
+++ 3516.py
@@ -0,0 +1,10 @@
+# 3516. Find Closest Person
+
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        if(abs(z-y)>abs(z-x)):
+            return 1
+        elif(abs(z-y)<abs(z-x)):
+            return 2
+        else:
+            return 0
